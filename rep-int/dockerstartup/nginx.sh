5d8f6e66648a8857e2f9784f10eb98dc90b2a0c3
Johann Gagnon-Bartsch <johanngb@umich.edu>
2020-06-30 08:26:41 -0400

    index.Rmd -> index.md

--- rep-int/dockerstartup/nginx.sh
+++ rep-int/dockerstartup/nginx.sh
@@ -4,8 +4,8 @@ else
     cp /dockerstartup/nginx_default.conf /etc/nginx/nginx.conf
 fi 
 if [ -f "/home/docker/docker/index.Rmd" ]; then
-    R -e 'rmarkdown::render("/home/docker/docker/index.Rmd", output_format="html_document", output_dir="/var/www/html", output_file="index.html")' &> /dev/null
+    R -e 'rmarkdown::render("/home/docker/docker/index.md", output_format="html_document", output_dir="/var/www/html", output_file="index.html")' &> /dev/null
 else 
-    R -e 'rmarkdown::render("/dockerstartup/index_default.Rmd", output_format="html_document", output_dir="/var/www/html", output_file="index.html")' &> /dev/null
+    R -e 'rmarkdown::render("/dockerstartup/index_default.md", output_format="html_document", output_dir="/var/www/html", output_file="index.html")' &> /dev/null
 fi 
 service nginx start &> /dev/null &
