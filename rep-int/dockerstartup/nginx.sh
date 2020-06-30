81ac67a98ca532dd3557210946fa3577eb880571
Johann Gagnon-Bartsch <johanngb@umich.edu>
2020-06-30 09:11:07 -0400

    fix Rmd -> md

--- rep-int/dockerstartup/nginx.sh
+++ rep-int/dockerstartup/nginx.sh
@@ -3,7 +3,7 @@ if [ -f "/home/docker/docker/nginx.conf" ]; then
 else 
     cp /dockerstartup/nginx_default.conf /etc/nginx/nginx.conf
 fi 
-if [ -f "/home/docker/docker/index.Rmd" ]; then
+if [ -f "/home/docker/docker/index.md" ]; then
     R -e 'rmarkdown::render("/home/docker/docker/index.md", output_format="html_document", output_dir="/var/www/html", output_file="index.html")' &> /dev/null
 else 
     R -e 'rmarkdown::render("/dockerstartup/index_default.md", output_format="html_document", output_dir="/var/www/html", output_file="index.html")' &> /dev/null
