4b4ff0ca01e9c9810e77706dc53d710af7a45d18
Johann Gagnon-Bartsch <johanngb@umich.edu>
2021-01-30 22:29:46 -0500

    no cache in home

--- rep-int/startup/jupyter.sh
+++ rep-int/startup/jupyter.sh
@@ -1,4 +1,3 @@
-mkdir -p /tmp/rep/jupyter
-mkdir -p /tmp/rep/local/share/jupyter
+mkdir -p /tmp/rep/cache /tmp/rep/jupyter /tmp/rep/local/share/jupyter 
 chown -R rep:rep /tmp/rep
-runuser - rep --command="export JUPYTER_CONFIG_DIR='/tmp/rep/jupyter' && export JUPYTER_DATA_DIR='/tmp/rep/local/share/jupyter' && jupyter lab --ip='*' --ServerApp.port=8889 --no-browser --ServerApp.token='' --ServerApp.base_url='/jupyter' &> /dev/null &"
+runuser - rep --command="export XDG_CACHE_HOME='/tmp/rep/cache' && export JUPYTER_CONFIG_DIR='/tmp/rep/jupyter' && export JUPYTER_DATA_DIR='/tmp/rep/local/share/jupyter' && jupyter lab --ip='*' --ServerApp.port=8889 --no-browser --ServerApp.token='' --ServerApp.base_url='/jupyter' &> /dev/null &"
