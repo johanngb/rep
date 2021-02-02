ae46ee4d64f891bb725fabcbdaeed5a9c5c93e36
Johann Gagnon-Bartsch <johanngb@umich.edu>
2021-02-02 17:40:36 -0500

    shiny.sh path bug

--- rep-int/startup/shiny.sh
+++ rep-int/startup/shiny.sh
@@ -1,6 +1,6 @@
 if [ -f "/home/rep/docker/startup/shiny-server.conf" ]; then
     cp /home/rep/docker/startup/shiny-server.conf /etc/shiny-server/shiny-server.conf
 else 
-    envsubst < ./shiny-server_default.conf > /etc/shiny-server/shiny-server.conf
+    envsubst < /startup/shiny-server_default.conf > /etc/shiny-server/shiny-server.conf
 fi 
 shiny-server &> /dev/null &
